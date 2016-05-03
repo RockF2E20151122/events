580a314c49f95ebcbf060d661e438faf983ecbac
unknown <rock20110414@gmail.com>
2016-05-03 16:33:04 +0800

    changes for test

--- WebContent/script/events.js
+++ WebContent/script/events.js
@@ -1,3 +1,44 @@
-$(function(){
-	alert('fuck')
+$(function() {
+	var log = function(eventObj) {
+		var pre = $("pre");
+		var html = pre.html();
+		pre.append( "EVENT (" + new Date().getTime() + "): " + eventObj.type + "\n");
+		// e.preventDefault();
+		/*
+		 * if (e.type == "touchmove") e.preventDefault();
+		 */
+		pre.scrollTop = pre.offsetHeight;
+	};
+	/**
+	 * 1. mouse events
+	 */
+	var $node = $('#nodeId');
+	$node.on("touchstart", function(){
+		var argu = arguments[0];
+		if(arguments.callee.caller.arguments.touches && arguments.callee.caller.arguments.touches.length>1){
+			console.log('fuck')
+		}
+		
+	});
+	$node.on("touchcancel", log);
+	$node.on("touchmove", log);
+	$node.on("touchleave", log);
+	$node.on("touchend", log);
+
+	$node.on("mouseover", log);
+	$node.on("mousemove", log);
+	$node.on("mousedown", log);
+	$node.on("mouseup", log);
+	$node.on("click", log);
+	$node.on("dblclick", log);
+	$node.on("context", log);
+
+	/**
+	 * 2. touch events
+	 */
+
+	/**
+	 * 3. touch screen
+	 */
+
 });
